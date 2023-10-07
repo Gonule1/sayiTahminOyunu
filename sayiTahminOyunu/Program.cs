869b8f261dfb288991b896b95f5b0d353722353f
Gonule1 <Gnlelmali33@gmail.com>
2023-10-07 15:35:29 +0300

    Proje dosyası ekle.

---
+++ sayiTahminOyunu/Program.cs
@@ -0,0 +1,27 @@
+﻿var rand= new Random(0); 
+int tutulan= rand.Next();
+int hak = 3;
+
+while (hak > 0)
+{
+    Console.WriteLine(tutulan);
+    Console.WriteLine("SAYI:");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+
+    hak--;
+    if (sayi == tutulan)
+    {
+        Console.WriteLine("tebrikler bildiniz");
+        break;
+    }
+    else
+    {
+        if (hak == 0)
+        {
+            Console.WriteLine("Hakkınız bitti");
+            break;
+        }
+        if (sayi > tutulan) { Console.WriteLine("aşağı in"); }
+        else { Console.WriteLine("yukarı çık"); }
+    }
+}
